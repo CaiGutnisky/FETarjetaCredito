196c361953a37a04c0c80e06331a12fd1d23eb0f
Cai Gutnisky <gutnisky04@gmail.com>
2022-11-07 14:58:09 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarjetaCreditoComponent } from './componentes/tarjeta-credito/tarjeta-credito.component';
 
+
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 
 @NgModule({
@@ -27,6 +33,12 @@ import {MatPaginatorModule} from '@angular/material/paginator';
     MatInputModule,
     MatIconModule,
     MatPaginatorModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/componentes/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
@@ -9,11 +10,22 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class TarjetaCreditoComponent implements OnInit {
 
+  listarTarjetas: any[] = ['Juan Perz', 0, '13/05' ]
   columnasBases: string[] = ['nombre', 'numero', 'fechaVencimiento'];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor() { }
+  form: FormGroup;
+
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
+
+  constructor (private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: [''],
+      numero: ['', [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
+      fechaVencimiento: [''],
+    })
+   }
 
   ngOnInit(): void {
   }
@@ -22,4 +34,15 @@ export class TarjetaCreditoComponent implements OnInit {
     this.paginator._intl.itemsPerPageLabel = 'items por pagina';
   }
 
+  agregarTarjeta() {
+    console.log(this.form)
+
+    const tarjeta: any = {
+      nombre: this.form.get('nombre')?.value,
+      numero: this.form.get('numero')?.value,
+      fechaVencimiento: this.form.get('fechaVencimiento')?.value,
+    }
+    this.listarTarjetas.push();
+    this.form.reset();
+  }
 }
