d134e4317bdc9f903e41c5ce46ad7a2cb6dd20ab
Cai Gutnisky <gutnisky04@gmail.com>
2022-11-07 11:20:23 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetaCreditoComponent } from './componentes/tarjeta-credito/tarjeta-credito.component';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaCreditoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/tarjeta-credito/tarjeta-credito.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-tarjeta-credito',
+  templateUrl: './tarjeta-credito.component.html',
+  styleUrls: ['./tarjeta-credito.component.scss']
+})
+export class TarjetaCreditoComponent implements OnInit {
+
+  columnasBases: string[] = ['nombre', 'numero', 'fechaVencimiento'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.paginator._intl.itemsPerPageLabel = 'items por pagina';
+  }
+
+}
